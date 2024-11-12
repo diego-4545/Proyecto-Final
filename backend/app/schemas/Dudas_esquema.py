9b21238e40e383fbc3c9a5175d31f33b1a626d93
diego-4545 <diegoalejandroescamillatorres@gmail.com>
2024-11-12 00:20:52 -0600

    Esquemas intento 2

---
+++ backend/app/schemas/Dudas_esquema.py
@@ -0,0 +1,25 @@
+# Librería para estructurar la API
+from pydantic import BaseModel
+### ============================= Dudas ==================================== ###
+
+# Obtener todos los IDs de dudas 
+class GET_TodasDudasRequest(BaseModel):
+
+
+class GET_TodasDudas_Response(BaseModel):
+    duda_ids: list[int]
+
+# Obtener información de una duda especidica
+class GET_InfoDudas_Request(BaseModel):
+    duda_id: int
+
+
+class GET_InfoDudasResponse(BaseModel):
+    # Columnas de la tabla dudas
+    duda_id: int
+    nombre: str 
+    email: str
+    descripcion: int
+    Imagen_del_problema: str # path to the image
+    fecha: int
+
