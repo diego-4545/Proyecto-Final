d18877da3cffc8c6d6f6cadcd33b0620a9bd80e9
diego-4545 <diegoalejandroescamillatorres@gmail.com>
2024-11-13 22:34:15 -0600

    Agregando cosas a el esquema de usuarios

---
+++ backend/app/data/dudas_queries.py
@@ -0,0 +1,60 @@
+import os
+from dotenv import load_dotenv
+import mysql.connector
+from mysql.connector import Error
+
+
+# Variables para la conexion de la BD
+#! Si no tienes el archivo .env y necesitas trabajar con la BD solicitar el archivo a algun miembro del equipo
+load_dotenv() # Esta función carga las variables de entorno localizadas en el archivo .env
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+# Funcion para conectarse a la BD y ejecutar las queries
+def articulos_query(query, parametros=None):
+    try:
+        # Conectar a la base de datos
+        conexion = mysql.connector.connect(
+            host=DB_HOST,        
+            user=DB_USER,       
+            password=DB_PASSWORD,
+            database=DB_NAME,
+        )
+
+        if conexion.is_connected():
+            cursor = conexion.cursor()
+            
+            # Ejecutar la consulta
+            if parametros:
+                cursor.execute(query, parametros)
+            else:
+                cursor.execute(query)
+
+            # Si es una consulta SELECT, obtener los resultados
+            if query.strip().upper().startswith("SELECT"):
+                resultados = cursor.fetchall()
+                return resultados
+            else:
+                # Para INSERT, UPDATE y DELETE. Solo ocupamos que se guarden los cambios
+                conexion.commit()
+                print("Consulta ejecutada exitosamente")
+        
+    except Error as e:
+        print("Error al ejecutar la consulta:", e)
+    finally:
+        # Cerramos la conexión
+        if conexion.is_connected():
+            cursor.close()
+            conexion.close()
+    return None
+
+
+### ============================= Dudas ==================================== ###
+# Seleccionar todos los ID de dudas 
+get_ids_dudas = "SELECT * FROM Dudas WHERE Dudas={dudas_id};"
+
+# Seleccionar la info de una duda específico
+get_info_duda = "SELECT * FROM Dudas WHERE Id={dudas_id};"
