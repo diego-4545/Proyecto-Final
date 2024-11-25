c8a17fafec526affb7856cb4dbc8871f7027a75b
diego-4545 <diegoalejandroescamillatorres@gmail.com>
2024-11-24 23:35:48 -0600

    cambiar foto de barra admin

--- frontend/admin/js/crud_articulos.js
+++ frontend/admin/js/crud_articulos.js
@@ -1,8 +1,61 @@
+import { verificarUsuario } from "./auth_ajax.js";
+
+var global_url = "http://127.0.0.1:8000"
+
+
+
 let currentPage = 1;
 const rowsPerPage = 5;
 
 
 let currentEditRow = null;
+
+// Conseguir informacion del usuarios
+async function get_usuario_info() {
+    const usuario_id = localStorage.getItem("usuario_id");
+
+    try {
+        const response = await $.ajax({
+            url: global_url + "/api/usuario/" + usuario_id,
+            method: "GET",
+            contentType: "application/json",
+        });
+
+        const usuario_info = {
+            "usuario_id": usuario_id,
+            "nombre": response.nombre,
+            "usuario": response.usuario,
+            "email": response.email,
+            "fecha": response.fecha,
+            "descripcion": response.descripcion,
+            "foto_perfil": response.foto_perfil,
+            "rol_id": response.rol_id,
+            "contraseña": response.contraseña,
+        };
+
+        cambiar_navbar_con_info_usuario(usuario_info);
+        cambiar_perfil_con_info_usuario(usuario_info);
+        cambiar_perfil_etiquetas_usuario();
+        cambiar_perfil_articulos_usuario();
+
+        return usuario_info; // Retorna los datos del usuario
+    } catch (error) {
+        console.error("Error al obtener los datos del usuario:", error);
+        throw error; // Lanza el error para manejarlo en el código que llama a esta función
+    }
+}
+
+
+// Cambiar info de la barra de navegación
+async function cambiar_navbar_con_info_usuario(usuario_info) {
+    // Cambiamos el nombre de usuario de la barra de navegación
+    $("#navbar-info-usuario-nombre").text(usuario_info.usuario);
+
+
+    // Cambiamos la imagen de perfil de la barra de navegación
+    $("#navbar-info-usuario-foto").attr("src", usuario_info.foto_perfil);
+}
+
 
 function editTag(button) {
     currentEditRow = $(button).closest('tr');
